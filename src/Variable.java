ed0b4802883a65e2e945cf4d41b7bb2edc99a9f8
no-cash-george <gpapjoin@gmail.com>
2026-04-24 00:47:13 +0300

    added starting files

---
+++ src/Variable.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Variable
+{
+    String nameOfVariable;
+    Utils.TypeEnum typeOfVariable;
+
+    public Variable(String nameOfVariable, Utils.TypeEnum typeOfVariable)
+    {
+        this.nameOfVariable = nameOfVariable;
+        this.typeOfVariable = typeOfVariable;
+    }
+}
