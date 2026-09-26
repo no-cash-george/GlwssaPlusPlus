b2b7ffcc12a24e21e00cb353954cb1709c237e08
no-cash-george <gpapjoin@gmail.com>
2026-09-26 14:39:37 +0300

    Finallized main

--- GlwssaPlusPlus IDE/src/Launcher.java
+++
@@ -1,7 +0,0 @@
-public class Launcher
-{
-    public static void main(String[] args) {
-        // This tricks the JVM into loading the JavaFX libraries dynamically
-        UI.main(args);
-    }
-}
--- src/TranspilationException.java
+++
@@ -1,7 +0,0 @@
-public class TranspilationException extends RuntimeException
-{
-    public TranspilationException(String message)
-    {
-        super(message);
-    }
-}
